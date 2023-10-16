068f1ea6e1ad9e6b5f0a619a4d7c52f85a3b8947
zqpaperpiano <ziqing.ong.2021@socsc.smu.edu.sg>
2023-10-16 14:40:16 +0800

    please

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require("dotenv").config();
 
-const PORT = 10000;
+const PORT = process.env.PORT;
 
 const register = require('./controllers/register');
 const signIn = require('./controllers/signin');
